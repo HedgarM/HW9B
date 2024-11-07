4175b9b6fabe1218808dac7c7cca31cbf5b9985c
Hedgar M <marinhedgar@yahoo.com>
2024-11-06 20:56:31 -0800

    Ex1 Complete

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require("multer");
 
 const app = express();
 const port = 3000;
+const upload = multer();
 
-app.get("/", (request, response) => {
-  response.send("Hello from Express!");
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.get('/form', (request, response) => {
-  response.sendFile(`${__dirname}/views/form.html`);
-})
-
-app.post('/submit', (req, res) => {
-  const { name, email } = req.body;
-  res.send(`
-      <h1>Form Submission Successful</h1>
-      <p>Thank you, <strong>${name}</strong>!</p>
-      <p>Your email address: <strong>${email}</strong></p>
-  `);
-});
+app.use(express.static("css"));
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+});
+
+app.get("/", (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get("/ex1", (request, response) => {
+  response.sendFile(`${__dirname}/views/ex1.html`);
+});
+
+app.post("/ex1", upload.array(), (request, response) => {
+
+  //For testing
+  console.log("============  FORM POST ============");
+  console.log("request body is: ", request.body);
+
+  const name = request.body.username;
+  const email = request.body.email;
+
+  response.send(`<strong>${name}</strong>, thank you for your order. We will keep you posted on delivery status at <strong>${email}</strong>.`)
 });
---
+++ public/ex1.js
@@ -0,0 +1,19 @@
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+
+  console.log("THIS IS RUNNING");
+  fetch("/ex1", {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.text())
+    .then(result => {
+      document.getElementById("result").innerHTML = result;
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
+})

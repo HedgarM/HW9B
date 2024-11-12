c6452d57cf79a3601df49181814b1630e70d015b
HedgarM <marinhedgar@yahoo.com>
2024-11-12 10:28:26 -0800

    Added Static "Views"

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use((req, res, next) => {
 
 app.use(express.static("css"));
 app.use(express.static("public"));
+app.use(express.static("views"));
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
